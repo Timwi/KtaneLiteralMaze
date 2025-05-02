ad96e3b3b92b182fcd041734d92e12f19a512c15
Blananas2 <45607696+Blananas2@users.noreply.github.com>
2025-05-02 01:46:38 -0400

    generate dummy letters and eliminate snakes

--- Assets/literalMazeScript.cs
+++ Assets/literalMazeScript.cs
@@ -17,6 +17,11 @@ public class literalMazeScript : MonoBehaviour {
 
     private MazeGenerator mazeGenerator;
     string mazeString;
+    int[] cellPositions = { 10, 12, 14, 16, 28, 30, 32, 34, 46, 48, 50, 52, 64, 66, 68, 70 };
+    int[] wallVectors = { -9, 1, 9, -1 };
+    List<string> cellWalls = new List<string> { };
+    List<string> distinctWalls = new List<string> { };
+    int attempts = 1;
 
     //Logging
     static int moduleIdCounter = 1;
@@ -32,20 +37,46 @@ public class literalMazeScript : MonoBehaviour {
 
     void Start () {
         mazeGenerator = new MazeGenerator(4);
-        //tryagain goes here
+        tryagain:
         mazeString = mazeGenerator.GenerateMaze();
+        int deadEnds = 0;
+        for (int p = 0; p < 16; p++) {
+            string theseWalls = "";
+            for (int v = 0; v < 4; v++) {
+                if (mazeString[cellPositions[p] + wallVectors[v]].ToString() == "█") {
+                    theseWalls += "NESW"[v];
+                }
+            }
+            if (theseWalls.Length == 3) { deadEnds++; }
+            cellWalls.Add(theseWalls);
+            if (!distinctWalls.Any(a => a == theseWalls)) {
+                distinctWalls.Add(theseWalls);
+                Letters[p].text = "abcdefghijklmnop"[distinctWalls.Count() - 1].ToString();
+            } else {
+                Letters[p].text = "abcdefghijklmnop"[distinctWalls.IndexOf(theseWalls)].ToString();
+            }
+        }
+        if (deadEnds == 2) {
+            attempts++;
+            Debug.Log("snake=" + cellWalls.Join(","));
+            cellWalls.Clear();
+            distinctWalls.Clear();
+            goto tryagain;
+        }
+        Debug.Log("attempts: " + attempts);
         LogMaze();
+        Debug.Log(cellWalls.Join(","));
     }
 
     void CellPress(KMSelectable Cell) {
-        for (int Q = 0; Q < Grid.Length; Q++) {
+        for (int Q = 0; Q < 16; Q++) {
             if (Grid[Q] == Cell) {
                 Debug.Log(Q);
             }
         }
     }
 
-     private void LogMaze()
+    private void LogMaze()
     {
         var maze = new string[9];
         for (int i = 0; i < maze.Length; i++)
